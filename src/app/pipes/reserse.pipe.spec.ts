7be7ca65b948014524dda38a6934375a1240e3e4
luiscaballero23 <luis_caballero23@hotmail.es>
2022-11-15 18:30:22 -0500

    primer commit

---
+++ src/app/pipes/reserse.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ResersePipe } from './reserse.pipe';
+
+describe('ResersePipe', () => {
+  it('create an instance', () => {
+    const pipe = new ResersePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
