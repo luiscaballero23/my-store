585f97c92071f955e9d592d76fdfce16e7855d2d
luiscaballero23 <luis_caballero23@hotmail.es>
2023-01-16 17:55:14 -0500

    clase 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,47 @@
 import { Component } from '@angular/core';
-import { Product } from './models/product.model';
+
+import { UsersService } from './services/users.service';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'my-store';
+  imgParent = '';
+  showImg = true;
+  token = '';
 
- 
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService
+  ) {}
+
+  onLoaded(img: string) {
+    console.log('log padre', img);
+  }
+
+  toggleImg() {
+    this.showImg = !this.showImg;
+  }
+
+  createUser() {
+    this.usersService
+      .create({
+        name: 'Luis',
+        email: 'luis@mail.com',
+        password: 'luis',
+      })
+      .subscribe((rta) => {
+        console.log(rta);
+      });
+  }
+
+  login() {
+    this.authService.login('luis@mail.com', 'luis').subscribe((rta) => {
+      console.log(rta.access_token);
+    });
+  }
 }
-
